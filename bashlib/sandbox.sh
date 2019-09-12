d049b985a443423f91299312dc9a7df1d101be57
Russell <russell@union-pos.com>
2019-09-12 12:38:16 -0500

    ignore error if repo does not have develop branch

--- bashlib/sandbox.sh
+++ bashlib/sandbox.sh
@@ -4,7 +4,9 @@ function git_clone_or_update() {
   if [ -d "$2/.git" ]; then
     action "update $1"
     ( cd "$2" || exit; git checkout master && git pull origin master && git submodule update --recursive > /dev/null 2>&1 )
+    set +e
     ( cd "$2" || exit; git checkout develop && git pull origin develop && git submodule update --recursive > /dev/null 2>&1 )
+    set -e
   else
     action "clone $1"
     git clone --recurse-submodules "$1" "$2" > /dev/null 2>&1
