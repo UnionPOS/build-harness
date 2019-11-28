a0d0d5cd380eed7dc05d9204905d4ecd1398cbb8
Russell <russell@union-pos.com>
2019-11-28 12:28:38 -0600

    remove bash commons from build harness

--- bin/install.sh
+++ bin/install.sh
@@ -12,13 +12,13 @@ fi
 echo "Cloning ${BUILD_HARNESS_REPO}#${BUILD_HARNESS_BRANCH}..."
 git clone -b "$BUILD_HARNESS_BRANCH" "$BUILD_HARNESS_REPO"
 
-export BASH_COMMONS_ORG=${4:-unionpos}
-export BASH_COMMONS_PROJECT=${5:-bash-commons}
-export BASH_COMMONS_BRANCH=${6:-master}
-export BASH_COMMONS_REPO="https://github.com/${BASH_COMMONS_ORG}/${BASH_COMMONS_PROJECT}.git"
+# export BASH_COMMONS_ORG=${4:-unionpos}
+# export BASH_COMMONS_PROJECT=${5:-bash-commons}
+# export BASH_COMMONS_BRANCH=${6:-master}
+# export BASH_COMMONS_REPO="https://github.com/${BASH_COMMONS_ORG}/${BASH_COMMONS_PROJECT}.git"
 
-(
-  cd "$BUILD_HARNESS_PROJECT" || exit
-  echo "Adding $BASH_COMMONS_PROJECT as subtree..."
-  git subtree add --prefix "$BASH_COMMONS_PROJECT" "$BASH_COMMONS_REPO" "$BASH_COMMONS_BRANCH" --squash
-)
+# (
+#   cd "$BUILD_HARNESS_PROJECT" || exit
+#   echo "Adding $BASH_COMMONS_PROJECT as subtree..."
+#   git subtree add --prefix "$BASH_COMMONS_PROJECT" "$BASH_COMMONS_REPO" "$BASH_COMMONS_BRANCH" --squash
+# )
