7a3d928c6a2670bc8cdd80d50232edf9fe2d0fa0
Russell Sherman <russell@union-pos.com>
2019-12-01 12:56:44 -0600

    add duty detection functions

--- bashlib/aws/ec2.sh
+++ bashlib/aws/ec2.sh
@@ -2,6 +2,32 @@
 
 # AWS ec2 functions
 
+function ec2_cluster_ips() {
+  local REGION="${1:-$INF_REGION}"
+  local TAG="${2:-$INF_INSTANCE_NAME}"
+  echo "ec2_cluster_ips $REGION $TAG"
+  aws ec2 describe-instances \
+    --query "Reservations[*].Instances[*].PrivateIpAddress" \
+    --filters "Name=tag:Name,Values=$TAG" \
+    --region "$REGION" \
+    --output text
+}
+export -f ec2_cluster_ips
+
+function ec2_is_duty_instance() {
+  IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+  CLUSTER=$(ec2_cluster_ips "$INF_REGION" "$INF_INSTANCE_NAME")
+  DUTY=$(echo "$CLUSTER" | xargs -n1 | sort -u | xargs | awk '{print $1}')
+
+  if [[ "$IP" != "$DUTY" ]]
+  then
+    echo "no"
+  else
+    echo "yes"
+  fi
+}
+export -f ec2_is_duty_instance
+
 function ec2_private_ip() {
   local INSTANCE_ID="$1"
 
