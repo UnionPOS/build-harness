0e632fb0a8e2fdc3c6480bd176196392407c31d0
Steve Heyman <steve@union-pos.com>
2020-01-06 16:59:53 -0600

    add function to draw pretty boxes around text

---
+++ bashlib/box_out.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Utility console function to draw a pretty colored box around the provided text.
+#
+# Usage:
+#         box_out "This is a message"
+#
+
+function box_out() {
+        input_char=$(echo "$@" | wc -c)
+        line=$(for i in `seq 0 $input_char`; do printf "-"; done)
+        # tput This should be the best option. what tput does is it will read the terminal info and render the correctly escaped ANSI code for you. code like \033[31m will break the readline library in some of the terminals.
+        tput bold
+        line="$(tput setaf 3)${line}"
+        space=${line//-/ }
+        echo " ${line}"
+        printf '|' ; echo -n "$space" ; printf "%s\n" '|';
+        printf '| ' ;tput setaf 4; echo -n "$@"; tput setaf 3 ; printf "%s\n" ' |';
+        printf '|' ; echo -n "$space" ; printf "%s\n" '|';
+        echo " ${line}"
+        tput sgr 0
+}
